0e18d8b20460d320fd2b87e0a7c13b67a095cdd7
Ashish Bansal <bansal.ashish096@gmail.com>
2015-11-11 01:25:13 +0530

    Removed unnecessary comments

--- index.php
+++ index.php
@@ -10,9 +10,7 @@
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
 
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="js/jquery-1.11.3.min.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
 </head>
 
