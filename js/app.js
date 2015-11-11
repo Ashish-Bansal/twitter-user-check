c2049b5912c36886e87296810a689dc53b734bf7
Ashish Bansal <bansal.ashish096@gmail.com>
2015-11-11 13:05:02 +0530

    Updated python_result.php to use new Python API

--- js/app.js
+++ js/app.js
@@ -7,21 +7,17 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
         templateUrl: '/none.php'
     });
     $routeProvider.when('/search_python/:search_user', {
-        templateUrl: function(attrs) {
-            return '/python_result.php?search_user=' + attrs.search_user;
-        },
-        resolve: {
-            app: function ($q) {
-                var defer = $q.defer();
-                defer.resolve();
-                return defer.promise;
-            }
-        }
+        templateUrl: '/result_template.html',
+        controller: 'JudgementResult'
     });
     $routeProvider.otherwise({
         redirectTo: '/'
     });
 }]);
+
+app.run(function ($templateCache, $http) {
+    $http.get('/result_template.html', { cache: $templateCache });
+});
 
 app.controller('FormController', ['$scope', '$location', function($scope, $location) {
     $scope.submit = function() {
@@ -35,5 +31,20 @@ app.controller('FormController', ['$scope', '$location', function($scope, $locat
 app.controller('ViewController', ['$scope', '$location', function($scope, $location) {
     $scope.$on('$viewContentLoaded', function(){
         console.log("View Content loaded");
+    });
+}]);
+
+app.controller('JudgementResult', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    $scope.fetched = false;
+    var judgeUrl = "/python_result.php/?search_user=".concat($routeParams.search_user);
+    $http.get(judgeUrl)
+    .success(function(data, status, headers, config) {
+        console.log("Result fetched");
+        $scope.fetched = true;
+        $scope.result = data;
+        console.log(data);
+    })
+    .error(function(data, status, headers, config) {
+        console.log("Error fetching Result!");
     });
 }]);
--- python_result.php
+++ python_result.php
@@ -1,6 +1,12 @@
 <?php
 
-$search_item = $_REQUEST['search_user'];
+$search_item = $_GET['search_user'];
+
+if (empty($search_item)) {
+	echo "User handle not passed";
+	die();
+}
+
 function generateRandomString($length = 8) {
 	$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	$charactersLength = strlen($characters);
@@ -31,40 +37,12 @@ $cmd_exec = exec($cmd_str, $op);
 
 $base_path = "fetched/";
 $file = fopen($base_path.$userfile,"r");
-$result_array = fgetcsv($file);
+$result_userinfo = fgets($file);
 fclose($file);
 
 $final_result = file_get_contents($base_path.$probfile);
 
-?>
+header('Content-Type: application/json');
+echo $result_userinfo;
 
-<section>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12" style="background-color:#5dc9e4">&nbsp;<br><br><br><br></div>
-		</div>
-		<div class="row" >
-			<div class="col-md-2">
-				<img src="<?php echo $result_array[9]; ?>" class="img-circle img-responsive" style="margin-top:-60px">
-			</div>
-			<div class="col-md-4" style="margin-top:-20px">
-				<h3><?php echo $result_array[3];?></h3>
-				<small>@<?php echo $result_array[1];?></small>
-				<br>
-				<h5><span class="glyphicon glyphicon-map-marker"></span> <?php echo $result_array[4];?></h5>
-				<h6>Joined on <?php echo $result_array[8];?></h6>
-			</div>
-			<div class="col-md-4" style="margin-top:-20px">
-				<h3><?php echo $result_array[5]; ?> <small>TWEETS</small></h3>
-				<h3><?php echo $result_array[6]; ?> <small>FOLLOWERS</small></h3>
-				<h3><?php echo $result_array[7]; ?> <small>FOLLOWING</small></h3>
-			</div>
-			<div class="col-md-2">
-				<br>
-				<button class="btn btn-primary btn-lg" type="button">
-					<?php echo $final_result; ?>
-				</button>
-			</div>
-		</div>
-	</div>
-</section>
+?>
